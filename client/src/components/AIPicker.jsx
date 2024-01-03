4a62944f9071de3d33964331ae7bfbff52dd4ca1
Mateus Souza <msouza954@gmail.com>
2024-01-03 15:31:14 -0300

    up

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,5 +1,47 @@
 import React from "react";
 
-export const AIPicker = () => {
-  return <div>AIPicker</div>;
+import { CustomButton } from "./CustomButton";
+
+export const AIPicker = ({
+  prompt,
+  setPrompt,
+  generatingImg,
+  handleSubmit,
+}) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Pergunte para a AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Perguntando para a IA..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+
+            <CustomButton
+              type="filled"
+              title="AI Completo"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
